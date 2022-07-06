2e23766b747300f310b337abf1e7ffe8bdf0fd2c
Chris Marshall <cmorrison82z@yahoo.com>
2022-07-06 17:28:27 +0100

    prepare to build

--- src/ObjectCache.lua
+++ src/ObjectCache.lua
@@ -1,51 +1,43 @@
------------------------------------------------------------
--------------------- MODULE DEFINITION --------------------
------------------------------------------------------------
-
 --[=[
-	@class ObjectCache
+    @class ObjectCache
 ]=]
-local ObjectCacheStatic = {}
-ObjectCacheStatic.__index = ObjectCacheStatic
+local ObjectCache = {}
+ObjectCache.__index = ObjectCache
 
 --[=[
-	@type Object
-	@within ObjectCache
+    @type Object BasePart | Model
+    @within ObjectCache
 
-	Generalization for things that are base parts or models.
+    Generalization for things that are base parts or models.
 ]=]
 export type Object = BasePart | Model
 
 --[=[
-	@interface ServiceDef
-	.Open {Object}
-	.InUse: {Object},
-	.CurrentCacheParent: Folder,
-	.Template: Object,
-	.ExpansionSize: number
-	@within ObjectCache
+    @interface OCache
+    .Open {Object}
+    .InUse: {Object},
+    .CurrentCacheParent: Folder,
+    .Template: Object,
+    .ExpansionSize: number
+    @within ObjectCache
 
-	Description
+    Description
 ]=]
-export type ObjectCache = {
-	Open: {Object},
-	InUse: {Object},
-	CurrentCacheParent: Folder,
-	Template: Object,
-	ExpansionSize: number
+export type OCache = {
+    Open: {Object},
+    InUse: {Object},
+    CurrentCacheParent: Folder,
+    Template: Object,
+    ExpansionSize: number
 }
 
 local objectProperties = {
-	"Transparency",
-	"Size",
-	"Material",
-	"Color",
-	"Reflectance"
-}
-
------------------------------------------------------------
------------------------ STATIC DATA -----------------------
------------------------------------------------------------					
+    "Transparency",
+    "Size",
+    "Material",
+    "Color",
+    "Reflectance"
+}	
 
 -- A CFrame that's really far away. Ideally. You are free to change this as needed.
 local CF_REALLY_FAR_AWAY = CFrame.new(0, 10e8, 0)
@@ -58,210 +50,202 @@ local ERR_NOT_INSTANCE = "Cannot statically invoke method '%s' - It is an instan
 -- Format params: paramName, expectedType, actualType
 local ERR_INVALID_TYPE = "Invalid type for parameter '%s' (Expected %s, got %s)"
 
------------------------------------------------------------
------------------------- UTILITIES ------------------------
------------------------------------------------------------
-
 --Similar to assert but warns instead of errors.
 local function wassert(requirement: boolean, messageIfNotMet: string)
-	if requirement == false then
-		warn(messageIfNotMet)
-	end
+    if requirement == false then
+        warn(messageIfNotMet)
+    end
 end
 
 local function FullWipe(t)
-	if not t then return end
-	
-	for i, v in pairs(t) do
-		if typeof(v) == "Instance" then
-			if v:IsA("Player") then
-				continue
-			end
-			v:Destroy()
-		elseif typeof(v) == "RBXScriptConnection" then
-			v:Disconnect()
-		elseif type(v) == "table" then
-			if type(v.Destroy) == "function" then
-				v:Destroy()
-			else
-				FullWipe(v)
-			end
-		end
-	end
-	
-	table.clear(t)
-	
-	t = nil
+    if not t then return end
+    
+    for i, v in pairs(t) do
+        if typeof(v) == "Instance" then
+            if v:IsA("Player") then
+                continue
+            end
+            v:Destroy()
+        elseif typeof(v) == "RBXScriptConnection" then
+            v:Disconnect()
+        elseif type(v) == "table" then
+            if type(v.Destroy) == "function" then
+                v:Destroy()
+            else
+                FullWipe(v)
+            end
+        end
+    end
+    
+    table.clear(t)
+    
+    t = nil
 end
 
 --Dupes a object from the template.
 local function MakeFromTemplate(self, template: Object, currentCacheParent: Instance): Object
-	local object: Object = template:Clone()
-	-- ^ Ignore W000 type mismatch between Instance and BasePart. False alert.
-	
-	object:PivotTo(CF_REALLY_FAR_AWAY)
-	
-	-- TODO : Further generalize for other instances ("Decals" for example) that can be reset on Return...
-	
-	if self._objectType == "BasePart" then
-		local selfTemplate = {}
-		
-		for _, propName in ipairs(objectProperties) do
-			selfTemplate[propName] = object[propName]
-		end
-		
-		self._selfTemplate[object] = selfTemplate
-	else
-		local selfTemplate = {}
-		
-		for _, bp in ipairs(object:GetDescendants()) do
-			if not bp:IsA("BasePart") then continue end
-			
-			local bpTemplate = {}
-			
-			for _, propName in ipairs(objectProperties) do
-				bpTemplate[propName] = bp[propName]
-			end
-			
-			selfTemplate[bp] = bpTemplate
-		end
-		
-		self._selfTemplate[object] = selfTemplate
-	end
-	
-	object.Parent = currentCacheParent
-	
-	return object
+    local object: Object = template:Clone()
+    -- ^ Ignore W000 type mismatch between Instance and BasePart. False alert.
+    
+    if self._objectType == "BasePart" then
+        local selfTemplate = {}
+        
+        for _, propName in ipairs(objectProperties) do
+            selfTemplate[propName] = object[propName]
+        end
+        
+        self._selfTemplate[object] = selfTemplate
+    else
+        local selfTemplate = {}
+        
+        for _, bp in ipairs(object:GetDescendants()) do
+            if not bp:IsA("BasePart") then continue end
+            
+            local bpTemplate = {}
+            
+            for _, propName in ipairs(objectProperties) do
+                bpTemplate[propName] = bp[propName]
+            end
+            
+            selfTemplate[bp] = bpTemplate
+        end
+        
+        self._selfTemplate[object] = selfTemplate
+    end
+    
+    object.Parent = currentCacheParent
+    
+    return object
 end
 
 --[=[
-	ObjectCache constructor.
+    @param template Object
+    @param numPrecreatedObjects number?
+    @param currentCacheParent Folder?
+    @return OCache
 
-	@param template Object
-	@param numPrecreatedObjects number?
-	@return ObjectCache
+    ObjectCache constructor.
 ]=]
-function ObjectCacheStatic.new(template: Object, numPrecreatedObjects: number?, currentCacheParent: Instance?): ObjectCache
-	numPrecreatedObjects = numPrecreatedObjects or 5
-	
-	if not currentCacheParent then
-		currentCacheParent = Instance.new("Folder")
-		currentCacheParent.Name = DEFAULT_CACHE_NAME:format(template.Name)
-		currentCacheParent.Parent = workspace
-	else
-		wassert(currentCacheParent.Parent == workspace, "Cache parent is not a member of workspace")
-	end
-	
-	--PrecreatedParts value.
-	--Same thing. Ensure it's a number, ensure it's not negative, warn if it's really huge or 0.
-	assert(numPrecreatedObjects > 0, "PrecreatedObjects can not be negative!")
-	wassert(numPrecreatedObjects ~= 0, "PrecreatedObjects is 0! This may have adverse effects when initially using the cache.")
-	wassert(template.Archivable, "The template's Archivable property has been set to false, which prevents it from being cloned. It will temporarily be set to true.")
-	
-	local oldArchivable = template.Archivable
-	template.Archivable = true
-	local newTemplate: Object = template:Clone()
-	
-	template.Archivable = oldArchivable
-	template = newTemplate
-	
-	local oCache: ObjectCache = {
-		Open = {},
-		InUse = {},
-		CurrentCacheParent = currentCacheParent,
-		Template = template,
-		ExpansionSize = 10,
-		_selfTemplate = {}, -- To reset a returned object to its original properties
-		_objectType = template:IsA("BasePart") and "BasePart" or "Model"
-	}
-	
-	setmetatable(oCache, ObjectCacheStatic)
-	
-	-- Below: Ignore type mismatch nil | number and the nil | Instance mismatch on the table.insert line.
-	for _ = 1, numPrecreatedObjects do
-		table.insert(oCache.Open, MakeFromTemplate(oCache, template, oCache.CurrentCacheParent))
-	end
-	oCache.Template.Parent = nil
-	
-	return oCache
+function ObjectCache.new(template: Object, numPrecreatedObjects: number?, currentCacheParent: Folder?): OCache
+    numPrecreatedObjects = numPrecreatedObjects or 5
+    
+    if not currentCacheParent then
+        currentCacheParent = Instance.new("Folder")
+        currentCacheParent.Name = DEFAULT_CACHE_NAME:format(template.Name)
+        currentCacheParent.Parent = workspace
+    else
+        wassert(currentCacheParent.Parent == workspace, "Cache parent is not a member of workspace")
+    end
+    
+    --PrecreatedParts value.
+    --Same thing. Ensure it's a number, ensure it's not negative, warn if it's really huge or 0.
+    assert(numPrecreatedObjects > 0, "PrecreatedObjects can not be negative!")
+    wassert(numPrecreatedObjects ~= 0, "PrecreatedObjects is 0! This may have adverse effects when initially using the cache.")
+    wassert(template.Archivable, "The template's Archivable property has been set to false, which prevents it from being cloned. It will temporarily be set to true.")
+    
+    local oldArchivable = template.Archivable
+    template.Archivable = true
+    local newTemplate: Object = template:Clone()
+    
+    template.Archivable = oldArchivable
+    template = newTemplate
+    newTemplate:PivotTo(CF_REALLY_FAR_AWAY)
+    
+    local oCache: OCache = {
+        Open = {},
+        InUse = {},
+        CurrentCacheParent = currentCacheParent,
+        Template = template,
+        ExpansionSize = 10,
+        _selfTemplate = {}, -- To reset a returned object to its original properties
+        _objectType = template:IsA("BasePart") and "BasePart" or "Model"
+    }
+    
+    setmetatable(oCache, ObjectCache)
+    
+    -- Below: Ignore type mismatch nil | number and the nil | Instance mismatch on the table.insert line.
+    for _ = 1, numPrecreatedObjects do
+        table.insert(oCache.Open, MakeFromTemplate(oCache, template, oCache.CurrentCacheParent))
+    end
+    oCache.Template.Parent = nil
+    
+    return oCache
 end
 
 --[=[
-	Retrieve an object from the cache
+    @return Object
 
-	@return Object
+    Retrieve an object from the cache
 ]=]
-function ObjectCacheStatic:GetObject(): Object
-	assert(getmetatable(self) == ObjectCacheStatic, ERR_NOT_INSTANCE:format("GetObject", "ObjectCache.new"))
-	
-	if #self.Open == 0 then
-		warn("No objects available in the cache! Creating [" .. self.ExpansionSize .. "] new object instance(s) - this amount can be edited by changing the ExpansionSize property of the ObjectCache instance... (This cache now contains a grand total of " .. tostring(#self.Open + #self.InUse + self.ExpansionSize) .. " objects.)")
-		for i = 1, self.ExpansionSize, 1 do
-			table.insert(self.Open, MakeFromTemplate(self, self.Template, self.CurrentCacheParent))
-		end
-	end
-	local object = self.Open[#self.Open]
-	self.Open[#self.Open] = nil
-	table.insert(self.InUse, object)
-	return object
+function ObjectCache:GetObject(): Object
+    assert(getmetatable(self) == ObjectCache, ERR_NOT_INSTANCE:format("GetObject", "ObjectCache.new"))
+    
+    if #self.Open == 0 then
+        warn("No objects available in the cache! Creating [" .. self.ExpansionSize .. "] new object instance(s) - this amount can be edited by changing the ExpansionSize property of the ObjectCache instance... (This cache now contains a grand total of " .. tostring(#self.Open + #self.InUse + self.ExpansionSize) .. " objects.)")
+        for i = 1, self.ExpansionSize, 1 do
+            table.insert(self.Open, MakeFromTemplate(self, self.Template, self.CurrentCacheParent))
+        end
+    end
+    local object = self.Open[#self.Open]
+    self.Open[#self.Open] = nil
+    table.insert(self.InUse, object)
+    return object
 end
 
 --[=[
-	Return object to cache. ObjectCache will attempt to reset important properties back to the template's properties.
+    @param object Object
 
-	@param object Object
+    Return object to cache. ObjectCache will attempt to reset important properties back to the template's properties.
 ]=]
-function ObjectCacheStatic:ReturnObject(object: Object)
-	assert(getmetatable(self) == ObjectCacheStatic, ERR_NOT_INSTANCE:format("ReturnObject", "ObjectCache.new"))
-	
-	local index = table.find(self.InUse, object)
-	
-	if index ~= nil then
-		table.remove(self.InUse, index)
-		table.insert(self.Open, object)
-		object:PivotTo(CF_REALLY_FAR_AWAY)
-		
-		if self._objectType == "BasePart" then
-			for propName, propVal in pairs(self._selfTemplate[object]) do
-				object[propName] = propVal
-			end
-		else
-			for objectDescendant, descProperties in pairs(self._selfTemplate[object]) do
-				for propName, propVal in pairs(descProperties) do
-					objectDescendant[propName] = propVal
-				end
-			end
-		end
-	else
-		error("Attempted to return object \"" .. object.Name .. "\" (" .. object:GetFullName() .. ") to the cache, but it's not in-use! Did you call this on the wrong object?")
-	end
+function ObjectCache:ReturnObject(object: Object)
+    assert(getmetatable(self) == ObjectCache, ERR_NOT_INSTANCE:format("ReturnObject", "ObjectCache.new"))
+    
+    local index = table.find(self.InUse, object)
+    
+    if index ~= nil then
+        table.remove(self.InUse, index)
+        table.insert(self.Open, object)
+        object:PivotTo(CF_REALLY_FAR_AWAY)
+        
+        if self._objectType == "BasePart" then
+            for propName, propVal in pairs(self._selfTemplate[object]) do
+                object[propName] = propVal
+            end
+        else
+            for objectDescendant, descProperties in pairs(self._selfTemplate[object]) do
+                for propName, propVal in pairs(descProperties) do
+                    objectDescendant[propName] = propVal
+                end
+            end
+        end
+    else
+        error("Attempted to return object \"" .. object.Name .. "\" (" .. object:GetFullName() .. ") to the cache, but it's not in-use! Did you call this on the wrong object?")
+    end
 end
 
 --[=[
    Expands the size of the cache.
-
-	@param amount number?
 ]=]
-function ObjectCacheStatic:Expand(amount: number?): ()
-	assert(getmetatable(self) == ObjectCacheStatic, ERR_NOT_INSTANCE:format("Expand", "ObjectCache.new"))
-	
-	amount = amount or self.ExpansionSize
-	
-	for i = 1, amount do
-		table.insert(self.Open, MakeFromTemplate(self, self.Template, self.CurrentCacheParent))
-	end
+function ObjectCache:Expand(amount: number?)
+    assert(getmetatable(self) == ObjectCache, ERR_NOT_INSTANCE:format("Expand", "ObjectCache.new"))
+    
+    amount = amount or self.ExpansionSize
+    
+    for i = 1, amount do
+        table.insert(self.Open, MakeFromTemplate(self, self.Template, self.CurrentCacheParent))
+    end
 end
 
 --[=[
    Destroys the object cache.
+
    :::caution
-	This will also destroy all parts it is responsible for.
-	:::
+    This will _also_ destroy all parts it is responsible for.
 ]=]
-function ObjectCacheStatic:Destroy()
-	assert(getmetatable(self) == ObjectCacheStatic, ERR_NOT_INSTANCE:format("Dispose", "ObjectCache.new"))
-	
-	FullWipe(self)
+function ObjectCache:Destroy()
+    assert(getmetatable(self) == ObjectCache, ERR_NOT_INSTANCE:format("Dispose", "ObjectCache.new"))
+    
+    FullWipe(self)
 end
 
-return ObjectCacheStatic
+return ObjectCache
